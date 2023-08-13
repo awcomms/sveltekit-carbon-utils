4c21d6eaff580a54b2f8b50e4130a750c7cce5d5
EdwardGold Edem Hogan <edge37@outlook.com>
2023-08-13 16:13:56 +0000

    .

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@
 export * from '$lib/components/index.js'
 export * from '$lib/types/index.js';
 export * from '$lib/util/index.js';
+export * from '$lib/stores.js';
--- src/lib/util/password/index.ts
+++
@@ -1,8 +0,0 @@
-export { encrypt_password } from "./encrypt_password.js";
-export { check_password } from "./check_password.js";
-
-export interface Encryption {
-	iv: string;
-	salt: string;
-	text: string;
-}
