d9bf9bc52b4e66bf3874a91f1edf50e1addeef63
EdwardGold Edem Hogan <edge37@outlook.com>
2023-08-16 00:59:28 +0000

    .

---
+++ update.js
@@ -0,0 +1,33 @@
+import { exec } from 'child_process';
+import { readFileSync, writeFileSync } from 'fs';
+import { inc } from 'semver';
+
+let mode = process.argv[2];
+console.log(process.argv)
+if (mode !== ('patch' || 'major' || 'minor')) {
+	console.error('invalid mode: ', mode);
+	process.abort(1);
+}
+
+let m = process.argv[3] || '.';
+
+const pj_path = './package.json';
+const pj = JSON.parse(readFileSync(pj_path));
+
+pj.version = inc(pj.version, mode);
+
+writeFileSync(pj_path, JSON.stringify(pj));
+
+const execs = (commands) => {
+	for (let i = commands.length - 1; i > -1; i--) {
+		exec(commands[i], (err, stdout, stderr) => {
+			console.log('std_out: ', stdout);
+			console.log('stderr: ', stderr);
+			if (err) {
+				throw err;
+			}
+		});
+	}
+};
+
+execs(['git add .', `git commit -m "${m}"`, 'git push', 'npm publish']);
