3b73f73e57dc9cb29a7f83aa16779a98e2e230db
EdwardGold Edem Hogan <edge37@outlook.com>
2023-10-08 18:12:09 +0000

    .

---
+++ src/lib/components/Search/index.ts
@@ -0,0 +1,2 @@
+export { default as Search } from './Search.svelte';
+export { default as SearchPagination } from './SearchPagination.svelte';
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,13 +1,10 @@
 export * from './Nav/index.js';
 export * from './Notification/index.js';
 export * from './pages/index.js'
+export * from './Search/index.js'
 export { default as Auth } from './Auth.svelte';
 export { default as Paystack } from './Paystack.svelte';
 export { default as OnEnter } from './OnEnter.svelte';
 export { default as Notification } from './Notification/Notification.svelte';
 export { default as Notifications } from './Notification/Notifications.svelte';
-export { default as ResultEntry } from './ResultEntry.svelte';
-export { default as Results } from './Results.svelte';
-export { default as ResultsPagination } from './ResultsPagination.svelte';
-export { default as Search } from './Search.svelte';
 export { default as VercelAnalyticsInject } from './VercelAnalyticsInject.svelte';
