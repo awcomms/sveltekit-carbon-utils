31ad7beef8c906fc1458cf2e47b73c622fccaa16
EdwardGold Edem Hogan <edge37@outlook.com>
2023-10-09 05:44:14 +0000

    .

--- src/lib/components/pages/Layout.svelte
+++ src/lib/components/pages/Layout.svelte
@@ -2,13 +2,12 @@
 	export let header_props: HeaderProps | undefined = undefined,
 		theme_switch = true,
 		offline_indicator = true;
-	import { Content, Grid, HeaderGlobalAction, Theme } from 'carbon-components-svelte';
+	import { Content, Grid, Theme } from 'carbon-components-svelte';
 	import 'carbon-components-svelte/css/all.css';
 	import Header from '$lib/components/Nav/Header/Header.svelte';
 	import SideNav from '$lib/components/Nav/SideNav.svelte';
 	import Notifications from '$lib/components/Notification/Notifications.svelte';
 	import { theme } from '$lib/util/theme_store.js';
-	import ConnectionSignalOff from 'carbon-icons-svelte/lib/ConnectionSignalOff.svelte';
 	import type { HeaderProps } from 'carbon-components-svelte/types/UIShell/Header.svelte.js';
 </script>
 
